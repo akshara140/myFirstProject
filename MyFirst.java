0b1116c3601a18edf9debb9ba095642f2dc10213
akshara140 <aksharatech14@gmail.com>
2025-05-18 15:40:32 -0400

    Update MyFirst.java

--- MyFirst.java
+++ MyFirst.java
@@ -5,7 +5,11 @@ class MyFirst
 
    public static void main(String arg[])
     {
-      System.out.println("helo");
+      System.out.println("Hello");
+       for(int i=0;i<=10;i++){
+          System.out.println(i);
+       }
+       System.out.println('Bye');
     }
 
 }
